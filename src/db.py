51d88cd0a1425dd0ce64f1172368fd0486c61b9f
Mnumzana Moyo <mfm222@cornell.edu>
2025-04-22 01:41:01 -0400

    Add relationships between tables

--- src/db.py
+++ src/db.py
@@ -2,6 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+course_instructors = db.Table(
+    "course_instructors",
+    db.Column("course_id", db.Integer, db.ForeignKey("courses.id")),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
+)
+
+course_students = db.Table(
+    "course_students",
+    db.Column("course_id", db.Integer, db.ForeignKey("courses.id")),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
+)
+
 class Course(db.Model):
     """
     Course Model
@@ -10,6 +22,10 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     code = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
+
+    assignments = db.relationship("Assignment", backref="course", cascade="all, delete-orphan")
+    instructors = db.relationship("User", secondary=course_instructors, backref="instructed_courses")
+    students = db.relationship("User", secondary=course_students, backref="enrolled_courses")
 
     def __init__(self, **kwargs):
         """
@@ -25,7 +41,10 @@ class Course(db.Model):
         return {
             "id": self.id,
             "code": self.code,
-            "name": self.name
+            "name": self.name,
+            "assignments": [a.serialize() for a in self.assignments],
+            "instructors": [i.serialize() for i in self.instructors],
+            "students": [s.serialize() for s in self.students]
         }
     
 class User(db.Model):
@@ -62,6 +81,7 @@ class Assignment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     due_date = db.Column(db.Integer, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable=False)
 
     def __init__(self, **kwargs):
         """
@@ -69,6 +89,7 @@ class Assignment(db.Model):
         """
         self.title = kwargs.get("title", "")
         self.due_date = kwargs.get("due_date", None)
+        self.course_id = kwargs.get("course_id")
 
     def serialize(self):
         """
