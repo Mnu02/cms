bc0b03e5f24b6df76165474e67ff769f5e03d391
Mnumzana Moyo <mfm222@cornell.edu>
2025-04-22 02:17:12 -0400

    Get all courses

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 from db import db
 from flask import Flask
+from db import db, Course, User, Assignment
+
+import json
 
 app = Flask(__name__)
 db_filename = "cms.db"
@@ -12,9 +15,19 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+def success_response(body, code=200):
+    return json.dumps(body), code
+
+def failure_response(message, code=404):
+    return json.dumps({'error': message}), code
 
 # your routes here
-
+@app.route("/api/courses/", methods=["GET"])
+def get_all_courses():
+    """
+    Get all courses
+    """
+    return success_response({"courses": [c.serialize() for c in Course.query.all()]})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
