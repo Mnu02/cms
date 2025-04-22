cb93ad858d60c2b5c81dec681febc4d4f8b198d7
Mnumzana Moyo <mfm222@cornell.edu>
2025-04-22 00:20:33 -0400

    Create baseline Course model

--- src/db.py
+++ src/db.py
@@ -2,4 +2,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# your classes here
+class Course(db.model):
+    """
+    Course Model
+    """
+    ___tablename___ = "courses"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    code = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a Course object
+        """
+        self.code = kwargs.get("code", "")
+        self.name = kwargs.get("name", "")
+
+    def serialize(self):
+        """
+        Serialize a Course object
+        """
+        return {
+            "id": self.id,
+            "code": self.code,
+            "name": self.name
+        }
